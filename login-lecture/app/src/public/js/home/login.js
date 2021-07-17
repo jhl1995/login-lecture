9378ee6d385497a20b0b484ad38fbda0439596b2
JAYNO <JAYNO@MacRentui-MacBookPro-2.local>
2021-07-17 18:40:59 +0900

    로그인을 요청하는 fetch 생성

--- login-lecture/app/src/public/js/home/login.js
+++ login-lecture/app/src/public/js/home/login.js
@@ -1,4 +1,25 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const { post } = require("../../../../app");
+
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button"); 
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+ const req = {
+   id : id.value,
+   psword : psword.value,
+ };
+ 
+ console.log(req);
+ fetch("/login",{
+   method : "POST", 
+   headers: {
+     "Content-Type" : "application/json"
+   },
+   body: JSON.stringify(req)
+ })
+}
