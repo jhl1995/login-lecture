410425a4b5eac9ecfc2ef098766170ed82201ace
JAYNO <JAYNO@MacRentui-MacBookPro-2.local>
2021-07-17 19:02:46 +0900

    백엔드에서 로그인 기능 구현

--- login-lecture/app/src/routes/home/home.ctrl.js
+++ login-lecture/app/src/routes/home/home.ctrl.js
@@ -1,15 +1,44 @@
 "use strict";
 
-const hello = (req, res) => {
-  res.render("home/index");
+const users = {
+  id : ["lee","이개발","김팀장"],
+  psword : ["1234","1234", "123456"],
+}
+
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
-const login = (req, res) => {
-  res.render("home/login");
+const process = {
+  login : (req,res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          sucess:true,
+        })
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
+  },
 };
+
 
 module.exports = {
-  hello,
-  login,
+  output,
+  process,
 };
 
