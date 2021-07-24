7f4979514ce4c813a953408ec8631a47fe4844dc
JAYNO <JAYNO@MacRentui-MacBookPro-2.local>
2021-07-24 22:41:53 +0900

    로그 관리 모듈 winston 등록 및 설정

--- login-lecture/app/bin/www.js
+++ login-lecture/app/bin/www.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const app = require("../app");
-const POST = process.env.PORT || 3000;
+const logger = require("../src/config/logger");
+const PORT = process.env.PORT || 3000;
 
-app.listen(POST,() => {
-  console.log("서버 가동");
+app.listen(PORT,() => {
+  logger.info(`${PORT} 서버에서 가동되었슴돠.`);
 }); 
